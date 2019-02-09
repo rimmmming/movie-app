e5c8c3a6f240eb5e3807cd4fab5d980a3c143150
rimmmming <jrim1307@gmail.com>
2019-02-09 21:30:08 +0900

    9th commit

---
+++ build/precache-manifest.c4ea71e1a93fbaf006cf420e5dc5a4eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "6f7fc525fd6dc63ee8dc",
+    "url": "/movie-app/static/js/main.6f7fc525.chunk.js"
+  },
+  {
+    "revision": "1faf785688f80209b35e",
+    "url": "/movie-app/static/js/1.1faf7856.chunk.js"
+  },
+  {
+    "revision": "6f7fc525fd6dc63ee8dc",
+    "url": "/movie-app/static/css/main.c6105ca1.chunk.css"
+  },
+  {
+    "revision": "d78b3b4f9b2b08023a6f7b3bf0e74ded",
+    "url": "/movie-app/index.html"
+  }
+];
